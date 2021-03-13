b94549195d6945c1f5bcad6f0aa7fa3be3553f47
ElenaChka <checkalina.lena@bk.ru>
2021-03-13 23:45:22 +0300

    firsi commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//Программа, отображающая числа, которые при делении на 4 дают  в остатке 3💓💓💓
+#include<iostream>
+using namespace std;
+int main()
+{
+int k;
+cout<<"Vvedite kolichestvo chisel :";
+cin>>k;
+for (int i=1;i<=k;i++)
+{
+    cout<<4*i+3<<endl;
+
+}
+}
